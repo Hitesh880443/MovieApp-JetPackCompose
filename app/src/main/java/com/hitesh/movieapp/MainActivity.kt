c983c77adbbc2ead5789aa8d2537cbba17b7a9c0
Hitesh Sutar <hsutar00@gmail.com>
2022-07-13 23:59:37 +0530

    Scaffold Implementation , Movie List UI

--- app/src/main/java/com/hitesh/movieapp/MainActivity.kt
+++ app/src/main/java/com/hitesh/movieapp/MainActivity.kt
@@ -1,28 +1,70 @@
 package com.hitesh.movieapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hitesh.movieapp.ui.theme.MovieAppJetpackComposeTheme
+import com.hitesh.movieapp.util.MOCK_MOVIE_LIST
+
+const val TAG = "Movie App"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppJetpackComposeTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    backgroundColor = Color.Gray,
+                    elevation = 4.dp
                 ) {
-                    Greeting("Android")
+                    Text(text = "Movie App")
+                }
+            }
+        ) {
+            content()
+        }
+    }
+
+}
+
+@Composable
+fun MainContent(movieList: List<String> = MOCK_MOVIE_LIST) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(it) { movie ->
+                    Log.d(TAG, "MainContent: $movie")
                 }
             }
         }
@@ -30,14 +72,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MovieRow(movie: String, onItemClick: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(top = 8.dp)
+            .clickable {
+                onItemClick.invoke(movie)
+            },
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp
+
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(80.dp)
+                    .padding(8.dp),
+                imageVector = Icons.Default.Person,
+                contentDescription = "Movie Icon"
+            )
+            Text(text = movie)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieAppJetpackComposeTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
---
+++ app/src/main/java/com/hitesh/movieapp/util/StringConstants.kt
@@ -0,0 +1,14 @@
+package com.hitesh.movieapp.util
+
+val MOCK_MOVIE_LIST = listOf(
+    "Inception",
+    "Goodfellas",
+    "Seven Samurai",
+    "Mission",
+    "Forrest Gump",
+    "Saving Private Ryan",
+    "Star Wars",
+    "Psycho",
+    "Parasite",
+    "Gladiator"
+)
